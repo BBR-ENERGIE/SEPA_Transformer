3094a1d3e279638c6c94c8a718924cf823c1357a
berengerbbr <berenger.mayoud@bbr-energie.fr>
2023-12-15 10:39:20 +0100

    functional v1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, redirect, render_template, send_from_directory
-from bs4 import BeautifulSoup
+from flask import Flask, request, render_template, send_from_directory
+import regex
 
 app = Flask(__name__)
 path = "uploads"
@@ -10,13 +10,11 @@ def index():
         file = request.files["virement"]
         file.save(f"{path}/{file.filename}")
         with open(f"{path}/{file.filename}") as f:
-            soup = BeautifulSoup(f, 'xml')
-            if soup.LclInstrm is not None:
-                soup.LclInstrm.decompose()
-            if soup.SeqTp is not None:
-                soup.SeqTp.decompose()
+            strfile = f.read()
+            strfile = regex.sub(r"\n\t*<LclInstrm>\X*<\/LclInstrm>", "", strfile)
+            strfile = regex.sub(r"\n\t*<SeqTp>\X*<\/SeqTp>", "", strfile)
 
             with open(f"{path}/ok{file.filename}", "w") as fp:
-                fp.write(str(soup.prettify()))
+                fp.write(strfile)
         return send_from_directory(path, f"ok{file.filename}", as_attachment=True)
     return render_template("index.html")
