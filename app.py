b9e3eecace29d4b5ea50d2d9bfd7d46c1832687a
berengerbbr <berenger.mayoud@bbr-energie.fr>
2023-12-20 18:18:18 +0100

    take both type of file into account

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from bs4 import BeautifulSoup
 from flask import Flask, request, render_template, send_from_directory
 import regex
+from HTMLParser import parser
 
 app = Flask(__name__)
 path = "uploads"
@@ -10,11 +12,28 @@ def index():
         file = request.files["virement"]
         file.save(f"{path}/{file.filename}")
         with open(f"{path}/{file.filename}") as f:
-            strfile = f.read()
-            strfile = regex.sub(r"\n\t*<LclInstrm>\X*<\/LclInstrm>", "", strfile)
-            strfile = regex.sub(r"\n\t*<SeqTp>\X*<\/SeqTp>", "", strfile)
+            soup = BeautifulSoup(f, "xml")
+            if soup.LclInstrm is not None:
+                soup.LclInstrm.decompose()
+            if soup.SeqTp is not None:
+                soup.SeqTp.decompose()
+            if soup.CtgyPurp is not None:
+                for tag in soup.find_all("CtgyPurp"):
+                    tag.decompose()
+            if soup.PmtInf.find("PmtTpInf", recursive=False) is None:
+                pmttpinf = soup.new_tag("PmtTpInf")
+                instrprty = soup.new_tag("InstrPrty")
+                instrprty.append("NORM")
+                svclvl = soup.new_tag("SvcLvl")
+                cd = soup.new_tag("Cd")
+                cd.append("SEPA")
+                svclvl.append(cd)
+                pmttpinf.append(instrprty)
+                pmttpinf.append(svclvl)
+
+                soup.PmtInf.find("CtrlSum", recursive=False).insert_after(pmttpinf)
 
             with open(f"{path}/ok{file.filename}", "w") as fp:
-                fp.write(strfile)
+                fp.write(str(soup))
         return send_from_directory(path, f"ok{file.filename}", as_attachment=True)
     return render_template("index.html")
